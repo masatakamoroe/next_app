52f5a3a63c955fe3c1dcf1badd83dbd988ead8c6
masataka moroe <masaozozoz91@gmail.com>
2021-02-08 19:34:24 +0900

    指摘分の直し

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import styles from '../../styles/Home.module.css'
-import HomeTemplate from "../components/templates/HomeTemplate"
+import styles from '../../styles/Home.module.css';
+import HomeTemplate from "components/templates/HomeTemplate";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <HomeTemplate></HomeTemplate>
+      <HomeTemplate />
     </div>
   );
 }
