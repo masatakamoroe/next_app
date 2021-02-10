1900c7e25c5e45da13d59ce26bb73479ef352d0b
masataka moroe <masaozozoz91@gmail.com>
2021-02-10 22:36:31 +0900

    指定内容修正

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,5 @@
-export {default as Checkbox} from "./Checkbox";
-export {default as PrimaryButton } from "./PrimaryButton";
-export {default as SecondaryButton} from "./SecondaryButton";
-export {default as Label} from "./Label";
-export {default as Textbox} from "./Textbox";
+export { default as Checkbox } from "./Checkbox";
+export { default as PrimaryButton } from "./PrimaryButton";
+export { default as SecondaryButton } from "./SecondaryButton";
+export { default as Label } from "./Label";
+export { default as Textbox } from "./Textbox";
