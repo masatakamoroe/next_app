e5320fa0d8fe52dac4e9c364d11c428ffa32b32e
masataka moroe <masaozozoz91@gmail.com>
2021-02-15 17:16:30 +0900

    指摘分修正

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -2,6 +2,6 @@ import { ForgotPasswordTemplate } from "components/templates";
 
 export default function Forgotpassword() {
     return (
-        < ForgotPasswordTemplate />
+        <ForgotPasswordTemplate />
     );
 };
