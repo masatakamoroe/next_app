dc82b7353acffe2c20a059831c9c4abf2b4fbafb
masataka moroe <masaozozoz91@gmail.com>
2021-02-25 19:12:10 +0900

    カレンダーデザインページ作成

--- src/components/templates/index.ts
+++ src/components/templates/index.ts
@@ -1,5 +1,6 @@
-export { default as HomeTemplate } from "./HomeTemplate";
-export { default as SignUpTemplate } from "./SignupTemplate";
-export { default as ForgotPasswordTemplate } from "./ForgotPasswordTemplate";
-export { default as TopTemplate } from "./TopTemplate";
-export { default as EditorTemplate } from "./EditorTemplate";
+export { default as HomeTemplate } from "./HomeTemplate"
+export { default as SignUpTemplate } from "./SignupTemplate"
+export { default as ForgotPasswordTemplate } from "./ForgotPasswordTemplate"
+export { default as TopTemplate } from "./TopTemplate"
+export { default as EditorTemplate } from "./EditorTemplate"
+export { default as CalendarTemplate } from "./CalendarTemplate"
---
+++ src/pages/calendar.tsx
@@ -0,0 +1,9 @@
+import { CalendarTemplate } from "components/templates";
+
+const Calendar: React.VFC = () => {
+    return (
+        <CalendarTemplate />
+    );
+};
+
+export default Calendar;
