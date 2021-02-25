03dc1d0f34ab762eca03409c02ed0bf96dbbb190
masataka moroe <masaozozoz91@gmail.com>
2021-02-25 22:38:04 +0900

    ディレクトリ名直し

--- src/pages/Signup.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUpTemplate } from "components/templates";
-
-const SignupPage: React.VFC = () => {
-    return (
-        <SignUpTemplate />
-    );
-};
-
-export default SignupPage;
--- src/pages/Top.tsx
+++
@@ -1,9 +0,0 @@
-import { TopTemplate } from "components/templates";
-
-const TopPage: React.VFC = () => {
-    return (
-        <TopTemplate />
-    );
-};
-
-export default TopPage;
