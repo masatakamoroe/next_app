53871cf093e723ecfcaeaec82e9cbc4091cc2655
masataka moroe <masaozozoz91@gmail.com>
2021-03-04 14:21:42 +0900

    api内容変更3

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,8 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default (req: any, res: any) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
   const id = req.query.id
